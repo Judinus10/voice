cee35065a8afc86fe3578a95006ebc9811b17bb9
judinus10 <judes.20222403@iit.ac.lk>
2024-08-21 10:53:02 +0530

    Add basic command listening functionality

--- VA.py
+++ VA.py
@@ -13,3 +13,19 @@ engine.setProperty('voice', voices[1].id)  # 1 for female and 0 for male voice
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print("User said:" + query + "\n")
+    except Exception as e:
+        print(e)
+        speak("I didnt understand")
+        return "None"
+    return query
